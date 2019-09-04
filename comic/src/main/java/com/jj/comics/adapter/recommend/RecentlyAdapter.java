45eb364591a02c4f45290bb46a45b8009e4a4367
wangzhiqiang <wangzhiq@ichile.com.cn>
2019-09-04 15:38:27 +0800

    最近更新

--- comic/src/main/java/com/jj/comics/adapter/recommend/RecentlyAdapter.java
+++ comic/src/main/java/com/jj/comics/adapter/recommend/RecentlyAdapter.java
@@ -25,15 +25,11 @@ public class RecentlyAdapter extends SimpleBaseAdapter<BookModel> {
             ILFactory.getLoader().loadNet(helper.<ImageView>getView(R.id.iv_recently_icon),
                     item.getCoverl(),
                     new RequestOptions()
-                            .transforms(new CenterCrop(), new RoundedCorners(Utils.dip2px(mContext, 5)))
+                            .transforms(new CenterCrop())
                             .error(R.drawable.img_loading)
                             .placeholder(R.drawable.img_loading));
             helper.<TextView>getView(R.id.tv_recently_name).setText(item.getTitle());
-            if (item.getFullflag() == 1) {//已完结
-                helper.<TextView>getView(R.id.tv_recently_update).setText("已完结");
-            } else {
-                helper.<TextView>getView(R.id.tv_recently_update).setText(String.format(mContext.getString(R.string.comic_update), item.getLastvolume()));
-            }
+//            helper.<TextView>getView(R.id.tv_recently_desc).setText(item.getIntro());
         }
 
     }
