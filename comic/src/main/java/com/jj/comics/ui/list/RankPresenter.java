95ca25e504d7f6882e2e419be7ed990621accfe9
wangzhiqiang <wangzhiq@ichile.com.cn>
2019-09-20 15:34:12 +0800

    删除无用文件

--- comic/src/main/java/com/jj/comics/ui/list/RankPresenter.java
+++
@@ -1,7 +0,0 @@
-package com.jj.comics.ui.list;
-
-import com.jj.base.mvp.BasePresenter;
-import com.jj.base.mvp.BaseRepository;
-
-public class RankPresenter extends BasePresenter<BaseRepository,RankContract.IRankView> implements RankContract.IRankPresenter{
-}
