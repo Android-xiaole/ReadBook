7c4f65dc768a98e6f7f4bb5ea89e707508f09fa4
wangzhiqiang <wangzhiq@ichile.com.cn>
2019-09-23 18:23:22 +0800

    增加通联支付

---
+++ comic/src/main/java/com/jj/comics/data/model/TLPayResponse.java
@@ -0,0 +1,13 @@
+package com.jj.comics.data.model;
+
+public class TLPayResponse extends ResponseModel {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
